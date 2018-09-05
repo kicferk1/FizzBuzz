3a93b44941da18243ab8b077ad9f9855fe86879b
kicferk1 <michal.kreft@softwire.com>
2018-09-05 10:51:21 +0100

    part 2

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,39 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
     class Program
     {
-        static string valuation1(int i)
+        static string Valuation2(int i)
         {
-            string value = "";
+            var result = new List<string>();
+
             if (i % 3 == 0)
             {
-                value = value + "Fizz";
+                result.Add("Fizz");
             }
 
             if (i % 5 == 0)
             {
-                value = value + "Buzz";
+                result.Add("Buzz");
             }
 
-
-            if (value == "")
+            if (i % 7 == 0)
             {
-                value = i.ToString();
+                result.Add("Bang");
             }
-            return value;
+
+            if (i % 11 == 0)
+            {
+                result.Clear();
+                result.Add("Bong");
+            }
+
+            if (i % 13 == 0)
+            {
+                bool inserted = false;
+                for (int j = 0; j < result.Count && !inserted; j++)
+                {
+                    if (result[j][0] == 'B')
+                    {
+                        result.Insert(j, "Fezz");
+                        inserted = true;
+                    }
+                }
+                if (!inserted) { result.Add("Fezz"); }
+            }
+
+            if (i % 17 == 0)
+            {
+                result.Reverse();
+            }
+
+
+
+            if (result.Count == 0)
+            {
+                result.Add(i.ToString());
+            }
+
+            string output = "";
+            foreach (var s in result)
+            {
+                output = output + s;
+            }
+
+            return output;
         }
+
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= 300; i++)
             {
-                Console.WriteLine(valuation1(i));
+                Console.WriteLine(Valuation2(i));
             }
             while (true) { }
         }
